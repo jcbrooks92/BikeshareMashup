f84667919f1344b0d3b6f3a9e5fb1e5b410389b4
Jeremy Brooks <lilman_2010@hotmail.com>
2016-09-24 20:14:07 -0500

    Google Maps Initial

---
+++ BikeShareBase/BikeShareBase/GoogleMapApi.js
@@ -0,0 +1,7 @@
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+}
+//# sourceMappingURL=GoogleMapApi.js.map
